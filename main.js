bb0d7d364f1b1439a2cd69b56883d071f0d1a9e9
Darek Sańczyk <tantyk@gmail.com>
2021-11-04 21:02:42 +0100

    zadanie 8 done

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
-console.log('start app') ;
-const os = require('os') ;
-const username = os.userInfo().username;
-console.log(username);
-const fs = require('fs');
-fs.writeFileSync('abc.txt', username);
+//  Zadanie 7
+
+// console.log('start app') ;
+// const os = require('os') ;
+// const username = os.userInfo().username;
+// console.log(username);
+// const fs = require('fs');
+// fs.writeFileSync('abc.txt', username);
+
+// Zadanie 8
+// const fs = require('fs');
+
+// const math = require('./math');
+// const a = fs.readFileSync('./a.txt','utf8');
+
+// console.log(a)
+
+// const b = fs.readFileSync('./b.txt','utf8');
+
+// console.log(b)
+
+
+// console.log("Dodawanie liczb" , a, "i" , b, "daje w wyniku", math.add(a, b));
+// console.log("Odejmowanie liczb", a, "i" , b,"daje w wyniku", math.sub(a, b));
+// console.log("Mnożenie liczb", a, "i" , b, "daje w wyniku", math.mul(a, b));
+// console.log("Dzielenie liczb", a, "i" , b, "daje w wyniku", math.div(a, b));
+
+// const resultAdd = "Dodawanie liczb " + a + " i " + b + " daje w wyniku " + math.add(a, b);
+// const resultSub = "Odejmowanie liczb " + a + " i " + b + " daje w wyniku " + math.sub(a, b);
+// const resultMul = "Mnożenie liczb " + a + " i " + b + " daje w wyniku " + math.mul(a, b);
+// const resultDiv = "Dzielenie liczb " + a + " i " + b + " daje w wyniku " + math.div(a, b);
+
+
+// fs.writeFileSync('wynik.txt', resultAdd + '\n');
+// fs.appendFileSync('wynik.txt', resultSub + '\n');
+// fs.appendFileSync('wynik.txt', resultMul + '\n');
+// fs.appendFileSync('wynik.txt', resultDiv + '\n');
+
+
+// Zadanie 9
